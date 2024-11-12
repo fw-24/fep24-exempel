b77bc16196327f71b4f1a539309aadeef7b39622
Fredrik Welander <welandfr@arcada.fi>
2024-11-12 10:41:21 +0200

    Lektion 3 mellancommit

---
+++ lektion-3/script.js
@@ -0,0 +1,15 @@
+function greeting() {
+    //const name = document.getElementById("namn").value;
+    const name = document.querySelector("#namn").value;
+    let greetingText = `Hello <b>${name}</b>! Ha en bra dag!`;
+
+    if (name == "Fredde" || name == "fredde") {
+        greetingText = `Hello <b>${name}</b>! Hej igen.`
+    } else if (name == "Jonny") {
+        greetingText = `Hello <b>${name}</b>! Kodat Unity på sistone?`
+    }
+    // document.querySelector("#greeting-output").innerText = greetingText;
+    // innerHTML tolkar HTML-kod i texten:
+    document.querySelector("#greeting-output").innerHTML = greetingText;
+
+}
